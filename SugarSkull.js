d1da413bd228272b9665dbeb785499936a8bb759
fernmicro <fernmicro@gmail.com>
2010-05-22 12:08:27 -0500

    whitespace cleanup

--- SugarSkull.js
+++ SugarSkull.js
@@ -2,86 +2,86 @@
 
 var APP = (typeof APP != "undefined") ? APP : {
 
-	overrides: {},
+    overrides: {},
 
-	override: function(ns, plan) {
-		overrides[ns] = plan;
-	},
+    override: function(ns, plan) {
+        overrides[ns] = plan;
+    },
 
-	exec: function(params) {
+    exec: function(params) {
 
-	    var strNS = params.ns;
-		var ns = {};
-	    var sectors = strNS.split('.');
+        var strNS = params.ns;
+        var ns = {};
+        var sectors = strNS.split('.');
 
-	    for (var i = 0; i < sectors.length; i++) {
-			var sector = sectors[i];
+        for (var i = 0; i < sectors.length; i++) {
+            var sector = sectors[i];
 
-			if (i == 0 && !window[sector]) {
-				window[sector] = {};
-				ns = window[sector];
-			}
-			else {
-				ns = ns[sector] = (ns[sector] ? ns[sector] : {});
-			}
-	    }
-	
-		delete this.Main;
-		eval(params.ns + " = this;");
+            if (i == 0 && !window[sector]) {
+                window[sector] = {};
+                ns = window[sector];
+            }
+            else {
+                ns = ns[sector] = (ns[sector] ? ns[sector] : {});
+            }
+        }
 
-		var self = this;
-		var methods = (typeof APP.overrides[ns] == "undefined") ? 
-			params.plan : APP.overrides[ns];
+        delete this.Main;
+        eval(params.ns + " = this;"); // TODO: there may be a better way to do this assignment.
 
-		for(method in methods) {
-			if(Object.prototype.toString.call(methods[method]) == "[object Array]") {
-				self[methods[method][0]].call(self, methods[method].slice(1, methods[method].length));
-			}
-			else {
-				self[methods[method]].call(self);			
-			}
-		}
-	}
+        var self = this;
+        var methods = (typeof APP.overrides[ns] == "undefined") ?
+            params.plan : APP.overrides[ns];
+
+        for(method in methods) {
+            if(Object.prototype.toString.call(methods[method]) == "[object Array]") {
+                self[methods[method][0]].call(self, methods[method].slice(1, methods[method].length));
+            }
+            else {
+                self[methods[method]].call(self);
+            }
+        }
+    }
 };
 
 // --- APPLICATION CODE ----------------------------------------------
 
  (function() {
 
-	/* private */
+    /* private */
 
-		/* code... */
+        /* code... */
 
-	/* public */ return {
+    /* public */ return {
 
-		Main: function() { /* the main entry point for the program, is fired automatically. */
+        Main: function() { /* the main entry point for the program, is fired automatically. */
 
-			APP.exec.call(this, {
-			/* some sugar to take care of namespacing, execution-context and code orginization. */
+            APP.exec.call(this, {
+            /* some sugar to take care of namespacing, execution-context and code orginization. */
 
-				ns: "myNS.foobar.bazz", /* the namespace that this code structure should be stored in. */
+                ns: "myNS.foobar.bazz", /* the namespace that this code structure should be stored in. */
 
-				plan: [ /* an array of names of functions that will be executed. */
+                plan: [ /* an array of names of functions that will be executed. */
 
-					["UnitOne", "foo", 13, function() {}] /* sample parameters */
-					,"UnitTwo"
-					,"UnitThree"
-				]
-			});
-		},
+                    ["UnitOne", "foo", 13, function() {}] /* sample parameters */
+                    ,"UnitTwo"
+                    ,"UnitThree"
+                ]
+            });
+        },
 
-		UnitOne: function(args) {
-			//console.log("asd")
-			console.dir(myNS.foobar.bazz)
-		},
+        UnitOne: function(args) {
+            //console.log("asd")
+            console.dir(myNS.foobar.bazz)
+        },
 
-		UnitTwo: function() {
-			/* code... */
-		},
+        UnitTwo: function() {
+            /* code... */
+        },
 
-		UnitThree: function() {
-			/* code... */
-		}					
+        UnitThree: function() {
+            /* code... */
+        }
 
-	}
+    }
 })().Main();
