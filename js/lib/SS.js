215764aee7e95e6c51ac20b8a88ca6567c97e410
Dave Geddes <davidcgeddes@gmail.com>
2011-08-04 15:16:17 -0600

    changed surrounding block to immediate function

--- js/lib/SS.js
+++ js/lib/SS.js
@@ -1,4 +1,4 @@
-;!function(window, undefined) {
+(function(window, undefined) {
 
   var dloc = document.location;
 
@@ -365,4 +365,4 @@
     onHashChanged:  function () {}
   };
  
-}(window);
+})(window);
