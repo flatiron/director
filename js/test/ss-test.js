ecc0ed1df86312d2230b2c16b808a3e94370fb5a
Dave Geddes <davidcgeddes@gmail.com>
2011-08-05 09:32:20 -0600

    changed the way tests were being run to avoid duplicate tests

--- js/test/ss-test.js
+++ js/test/ss-test.js
@@ -1,41 +1,49 @@
+var router = new Router({
+    '/cool': function(){
+      message = "cool";
+    },
+    '/say\\/([a-z0-9-]+)\\/([a-z0-9-]+)': function(one, two){
+      message = "awesome";
+      param1 = one;
+      param2 = two;
+    }
+}).init();
+
+var message;
+var param1;
+var param2;
+
 //setup/takedown
 module("SugarSkull.js", {
   setup: function() {
+//    console.log('setup');
     window.location.hash = "";
+    message = "changeme";
+    param1 = "changeme";
+    param2 = "changeme";
   },
-  teardown: function(){}
+  teardown: function(){
+//    console.log('teardown');
+    window.location.hash = "";
+  }
 });
 
 asyncTest("basic match", function(){
-  var message = "changeme";
-  var router = Router({
-    '/cool': function(){
-      message = "cool";
-    }
-  }).init();
   window.location.hash = "/cool";
-  
   setTimeout(function(){
-    equals(message, "cool", "basic route should have changed variable");
     start();
-  }, 20);
+    equals(message, "cool", "basic route should have changed variable");
+  }, 10);
 });
 
 asyncTest("regex match", function(){
-  var message = "changeme";
-  var router = Router({
-    '/initiative\\/([a-z0-9-]+)\\/([a-z0-9-]+)': function(one, two){
-      message = "awesome";
-      equals(one, "hi");
-      equals(two, "there");
-    }
-  }).init();
-  window.location.hash = "/initiative/hi/there";
-
+  window.location.hash = "/say/hi/there";
   setTimeout(function(){
-    equals(message, "awesome", "basic route should have changed variable");
+    equals(message, "awesome", "route should have changed the message variable");
+      equals(param1, "hi");
+      equals(param2, "there");
     start();
-  }, 20);
+  }, 10);
 });
 
 //todo test on, after, and notfound
