0425ffee4f0a2f9d582cb70aeb8b3500ec61cdcc
hij1nx <fernmicro@gmail.com>
2011-09-26 17:19:53 -0400

    [api dist test] First pass as normalizing routes through ".mount()"

--- test/mount-test.js
+++ test/mount-test.js
@@ -1,7 +1,7 @@
 
-
 var assert = require('assert'),
     vows = require('vows'),
+    eyes = require('eyes'),
     director = require('../lib/director');
     
 vows.describe('director/router/mount').addBatch({
@@ -11,16 +11,24 @@ vows.describe('director/router/mount').addBatch({
       "should sanitize the routes correctly": function (router) {
         function foobar () { }
         function foostar () { }
+        function foobazzbuzz () { }
       
         router.mount({
           '/foo': {
             '/bar': foobar,
             '/*': foostar
+          },
+          '/foo/bazz': {
+            '/buzz': foobazzbuzz
           }
         });
-      
+
+        eyes.inspect(router.routes);
         assert.strictEqual(router.routes.foo.bar, foobar);
         assert.strictEqual(router.routes.foo['([_.()!\\ %@&a-zA-Z0-9-]+)'], foostar);
+        
+        assert.isObject(router.routes.foo.bazz);
+        assert.strictEqual(router.routes.foo.bazz.buzz, foobazzbuzz);
       }
     }
   }
