85f2a7b710391462ccb29377a976a9e6a45815b2
indexzero <charlie.robbins@gmail.com>
2011-09-27 09:32:36 -0400

    [api test] Added `Router.prototype.param()`, relevant helper functions, and tests

--- test/mount-test.js
+++ test/mount-test.js
@@ -3,42 +3,80 @@ var assert = require('assert'),
     vows = require('vows'),
     eyes = require('eyes'),
     director = require('../lib/director');
-    
+
+function assertRoute (fn, path, route) {
+  if (path.length === 1) {
+    return assert.strictEqual(route[path.shift()], fn);
+  }
+
+  route = route[path.shift()];
+  assert.isObject(route);
+  assertRoute(fn, path, route);
+}
+
 vows.describe('director/router/mount').addBatch({
   "An instance of director.Router": {
-    topic: new director.Router(),
-    "the mount() method": {
-      "should sanitize the routes correctly": function (router) {
-        function foobar () { }
-        function foostar () { }
-        function foobazzbuzz () { }
-        var fnArray = [foobar, foostar];
-      
-        router.mount({
-          '/foo': {
-            '/bar': foobar,
-            '/*': foostar,
-            '/jitsu/then': {
-              before: foobar
-            }
-          },
-          '/foo/bazz': {
-            '/buzz': foobazzbuzz
-          },
-          '/foo/jitsu': {
-            '/then': fnArray
-          },
-          '/foo/jitsu/then/now': foostar
-        });
+    "with no preconfigured params": {
+      topic: new director.Router(),
+      "the mount() method": {
+        "should sanitize the routes correctly": function (router) {
+          function foobar () { }
+          function foostar () { }
+          function foobazzbuzz () { }
+          function foodog () { }
+          var fnArray = [foobar, foostar];
 
-        assert.strictEqual(router.routes.foo.bar.on, foobar);
-        assert.strictEqual(router.routes.foo['([_.()!\\ %@&a-zA-Z0-9-]+)'].on, foostar);
-        assert.strictEqual(router.routes.foo.jitsu.then.on, fnArray);
-        assert.strictEqual(router.routes.foo.jitsu.then.before, foobar);
+          router.mount({
+            '/foo': {
+              '/bar': foobar,
+              '/*': foostar,
+              '/jitsu/then': {
+                before: foobar
+              }
+            },
+            '/foo/bazz': {
+              '/buzz': foobazzbuzz
+            },
+            '/foo/jitsu': {
+              '/then': fnArray
+            },
+            '/foo/jitsu/then/now': foostar,
+            '/foo/:dog': foodog
+          });
+
+          assertRoute(foobar,      [ 'foo', 'bar', 'on'],                       router.routes);
+          assertRoute(foostar,     ['foo', '([_.()!\\ %@&a-zA-Z0-9-]+)', 'on'], router.routes);
+          assertRoute(fnArray,     ['foo', 'jitsu', 'then', 'on'],              router.routes);
+          assertRoute(foobar,      ['foo', 'jitsu', 'then', 'before'],          router.routes);
+          assertRoute(foobazzbuzz, ['foo', 'bazz', 'buzz', 'on'],               router.routes);
+          assertRoute(foostar,     ['foo', 'jitsu', 'then', 'now', 'on'],       router.routes);
+          assertRoute(foodog,      ['foo', '([a-zA-Z0-9-]+)', 'on'],            router.routes);
+        }
+      }
+    },
+    "with preconfigured params": {
+      topic: function () {
+        var router = new director.Router();
+        router.param('city', '([\\w\\-]+)');
+        router.param(':country', /([A-Z][A-Za-z]+)/);
+        router.param(':zip', /([\d]{5})/);
+        return router;
+      },
+      "should sanitize the routes correctly": function (router) {
+        function usaCityZip () { }
+        function countryCityZip () { }
         
-        assert.isObject(router.routes.foo.bazz);
-        assert.strictEqual(router.routes.foo.bazz.buzz.on, foobazzbuzz);
-        assert.strictEqual(router.routes.foo.jitsu.then.now.on, foostar);
+        router.mount({
+          '/usa/:city/:zip': usaCityZip,
+          '/world': {
+            '/:country': {
+              '/:city/:zip': countryCityZip
+            }
+          }
+        });
+        
+        assertRoute(usaCityZip, ['usa', '([\\w\\-]+)', '([\\d]{5})', 'on'], router.routes);
+        assertRoute(countryCityZip, ['world', '([A-Z][A-Za-z]+)', '([\\w\\-]+)', '([\\d]{5})', 'on'], router.routes);
       }
     }
   }
