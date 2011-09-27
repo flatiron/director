32f4c1f8b36a2b2fa71749e88f2a578860a53261
indexzero <charlie.robbins@gmail.com>
2011-09-27 04:39:42 -0400

    [api test] First pass at `dispatch()`

---
+++ test/dispatch-test.js
@@ -0,0 +1,37 @@
+
+var assert = require('assert'),
+    vows = require('vows'),
+    eyes = require('eyes'),
+    director = require('../lib/director');
+    
+vows.describe('director/router/mount').addBatch({
+  "An instance of director.Router": {
+    topic: new director.Router({
+      '/foo': {
+        '/bar': {
+          '/buzz': function log () { console.log('foo bar buzz') }
+        }
+      },
+      '/f*': {
+        '/barbie': function log () { console.log ('f* barbie') }
+      }
+    }),
+    "should have the correct routing table": function (router) {
+      assert.isObject(router.routes.foo);
+      assert.isObject(router.routes.foo.bar);
+      assert.isObject(router.routes.foo.bar.buzz);
+      assert.isFunction(router.routes.foo.bar.buzz.on);
+    },
+    "the dispatch() method": {
+      "/foo/bar/buzz": function (router) {
+        assert.isTrue(router.dispatch('on', '/foo/bar/buzz'));
+      },
+      "/foo/BAD": function (router) {
+        assert.isFalse(router.dispatch('on', '/foo/BAD'));
+      },
+      "/bar/bar": function (router) {
+        assert.isFalse(router.dispatch('on', '/bar/bar'));
+      }
+    }
+  }
+}).export(module);
