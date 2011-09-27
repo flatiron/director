e00b2ca2fb62a3ae8639543b4feb08ed321280b5
indexzero <charlie.robbins@gmail.com>
2011-09-27 05:36:13 -0400

    [refactor] Optimize the dispatch algorithm to be a pure bottom-up tree traversal with no need to pass an Array of functions

--- test/dispatch-test.js
+++ test/dispatch-test.js
@@ -8,12 +8,14 @@ vows.describe('director/router/mount').addBatch({
   "An instance of director.Router": {
     topic: new director.Router({
       '/foo': {
-        before: function () { console.log('before foo') },
-        on: function log () { console.log('foo') },
+        before: function beforeFoo () { console.log('before foo') },
+        on: function onFoo () { console.log('foo') },
+        after: function afterFoo () { console.log('after foo') },
         '/bar': {
-          before: function () { console.log('before foo bar') },
-          on: function log () { console.log('foo bar') },
-          '/buzz': function log () { console.log('foo bar buzz') }
+          before: function beforeFooBar () { console.log('before foo bar') },
+          on: function onFooBar () { console.log('foo bar') },
+          after: function afterFooBar () { console.log ('after foo bar') },
+          '/buzz': function onFooBarBuzz () { console.log('foo bar buzz') }
         }
       },
       '/f*': {
@@ -29,6 +31,9 @@ vows.describe('director/router/mount').addBatch({
     "the dispatch() method": {
       "/foo/bar/buzz": function (router) {
         assert.isTrue(router.dispatch('on', '/foo/bar/buzz'));
+      },
+      "/foo/barbie": function (router) {
+        assert.isTrue(router.dispatch('on', '/foo/barbie'));
       },
       "/foo/BAD": function (router) {
         assert.isFalse(router.dispatch('on', '/foo/BAD'));
