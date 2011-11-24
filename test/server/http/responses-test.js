bea9fc6522b5b70decca25fff13ab647205765a2
indexzero <charlie.robbins@gmail.com>
2011-11-23 23:39:06 -0500

    [api test] Expose HTTP responses on `director.http` not `director.http.responses` and add more test coverage

--- test/server/http/responses-test.js
+++ test/server/http/responses-test.js
@@ -5,3 +5,17 @@
  * MIT LICENSE
  *
  */
+ 
+var assert = require('assert'),
+    vows = require('vows'),
+    director = require('../../../lib/director');
+    
+vows.describe('director/http/responses').addBatch({
+  "When using director.http": {
+    "it should have the relevant responses defined": function () {
+      Object.keys(require('../../../lib/director/http/responses')).forEach(function (name) {
+        assert.isFunction(director.http[name]);
+      });
+    }
+  }
+}).export(module);
