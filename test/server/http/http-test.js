7f14d248e612874078f35771d883a52a383c4255
indexzero <charlie.robbins@gmail.com>
2011-11-29 20:21:41 -0500

    [test] Added test coverage for gh-53

--- test/server/http/http-test.js
+++ test/server/http/http-test.js
@@ -28,6 +28,19 @@ function createServer (router) {
   });
 }
 
+function assertGet (uri) {
+  return {
+    topic: function () {
+      request({ uri: 'http://localhost:9090/' + uri }, this.callback);
+    },
+    "should respond with `hello from (bark)`": function (err, res, body) {
+      assert.isNull(err);
+      assert.equal(res.statusCode, 200);
+      assert.equal(body, 'hello from (bark)')
+    }
+  }
+}
+
 vows.describe('director/server/http').addBatch({
   "An instance of director.http.Router": {
     "instantiated with a Routing table": {
@@ -43,19 +56,17 @@ vows.describe('director/server/http').addBatch({
       "when passed to an http.Server instance": {
         topic: function (router) {
           router.get(/foo\/bar\/(\w+)/, helloWorld);
+          router.path(/bar\/bazz\//, function () {
+            this.get(/(\w+)/, helloWorld)
+          });
           
           var server = createServer(router),
               that = this;
               
-          server.listen(9090, function () {
-            request({ uri: 'http://localhost:9090/foo/bar/bark' }, that.callback);
-          });
+          server.listen(9090, this.callback);
         },
-        "should respond with `hello from (bark)`": function (err, res, body) {
-          assert.isNull(err);
-          assert.equal(res.statusCode, 200);
-          assert.equal(body, 'hello from (bark)')
-        }
+        "a request to foo/bar/bark": assertGet('foo/bar/bark'),
+        "a request to bar/bazz/bark": assertGet('bar/bazz/bark')
       }
     }
   }
