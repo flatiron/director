c7cc95bb83689f21b041e8b855cabd21857f3a2b
Thomas Blobaum <tblobaum@gmail.com>
2012-01-16 20:31:47 +0100

    [fix test] Fix core tests to reflect new `RegExp`

--- test/server/core/mount-test.js
+++ test/server/core/mount-test.js
@@ -71,7 +71,7 @@ vows.describe('director/router/mount').addBatch({
           assertRoute(foobar,      ['foo', 'jitsu', 'then', 'before'],          router.routes);
           assertRoute(foobazzbuzz, ['foo', 'bazz', 'buzz', 'on'],               router.routes);
           assertRoute(foostar,     ['foo', 'jitsu', 'then', 'now', 'on'],       router.routes);
-          assertRoute(foodog,      ['foo', '([a-zA-Z0-9-]+)', 'on'],            router.routes);
+          assertRoute(foodog,      ['foo', '([._a-zA-Z0-9-]+)', 'on'],          router.routes);
         }
       }
     },
