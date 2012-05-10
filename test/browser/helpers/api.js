d582241dd8729b7480cd09e9c72d324408096803
Vesa Poikajärvi <vesa.poikajarvi@leonidasoy.fi>
2012-05-10 15:54:09 +0300

    HTML5 mode testing needs timeout in the calls because of the timeout used when creating onpopstate method

--- test/browser/helpers/api.js
+++ test/browser/helpers/api.js
@@ -16,6 +16,16 @@ function createTest(name, config, use, test) {
     test = use;
     use = undefined;
   }
+
+  var innerTimeout = 0;
+  if (HTML5TEST) {
+    if (use === undefined) {
+      use = {};
+    }
+    use.html5history = true;
+    innerTimeout     = 500; // Because of the use of setTimeout when defining onpopstate
+  }
+
   asyncTest(name, function() {
     setTimeout(function() {
       var router = new Router(config),
@@ -27,19 +37,25 @@ function createTest(name, config, use, test) {
 
       router.init();
 
-      test.call(context = {
-        router: router,
-        navigate: function(url, callback) {
-          window.location.hash = url;
-          setTimeout(function() {
-            callback.call(context);
-          }, 14);
-        },
-        finish: function() {
-          router.destroy();
-          start();
-        }
-      });
+      setTimeout(function() {
+        test.call(context = {
+          router: router,
+          navigate: function(url, callback) {
+            if (HTML5TEST) {
+              router.setRoute(url);
+            } else {
+              window.location.hash = url;
+            }
+            setTimeout(function() {
+              callback.call(context);
+            }, 14);
+          },
+          finish: function() {
+            router.destroy();
+            start();
+          }
+        })
+      }, innerTimeout);
     }, 14);
   });
 };
